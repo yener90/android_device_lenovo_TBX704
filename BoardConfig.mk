ec9d3db26c1e92d2ff7d79637ad96157c4922f42
Yener <yener90@git.com>
2019-03-30 11:58:18 +0100

    TBX704: Updated command lines
    Change-Id: Idaef88b6d3201b759bc5d00be2aa7db808ec8cdf

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -144,7 +144,7 @@ TARGET_RECOVERY_DEVICE_MODULES := libinit_tbx704
 
 # Kernel
 BOARD_KERNEL_BASE := 0x80000000
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlycon=msm_hsl_uart,0x78af000 androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 androidboot.selinux=permissive
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_OFFSET = 0x00008000
 BOARD_KERNEL_PAGESIZE := 2048
